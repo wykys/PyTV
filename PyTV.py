f11929c7856527565ae47d3ceecb58b99def50b9
wykys <vvykys@gmail.com>
2015-10-22 01:38:30 +0200

    v3

--- PyTV.py
+++ PyTV.py
@@ -26,15 +26,22 @@ class PyTV:
     "PyTV initialization"    
     def __init__(self):
         self.win = tk.Tk()        
-        self.win.title("PyTV ɔ CC - created by wykys 2015")        
+        self.win.title("PyTV ɔ CC - created by wykys 2015")
+        self.StationInLine = 6
+        self.row = 0
+        self.column = 0
     "Play select station in vlc"
     def play(self, addr):
         os.system("killall vlc")
-        os.system("cvlc " + addr + " &")
+        os.system("cvlc {0} &".format(addr))
     "Add station"
-    def addStaion(self, name=False, addr=False):
+    def addStation(self, name=False, addr=False):
         if type(name) == str and type(addr) == str:
-            tk.Button(self.win, text=name, font="Courier 15 bold", command=lambda a=addr: self.play(a)).pack(side=tk.LEFT, padx=2)
+            self.column += 1
+            if self.column > self.StationInLine:
+                self.row += 1
+                self.column = 1
+            tk.Button(self.win, text=name, font="Courier 15 bold", command=lambda a=addr: self.play(a)).grid(row=self.row, column=self.column, padx=2, pady=2, sticky=tk.W+tk.E)
     "Run PyTV GUI"
     def runGUI(self):
         self.win.mainloop()    
@@ -43,13 +50,42 @@ class PyTV:
         os.system("killall vlc")
 
 app = PyTV()
-app.addStaion("ČT1",    "udp://@239.255.11.1:1234")
-app.addStaion("ČT2",    "udp://@239.255.11.2:1234")
-app.addStaion("ČT24",   "udp://@239.255.11.3:1234")
-app.addStaion("ZOOM",   "udp://@239.255.13.3:1234")
-app.addStaion("COOL",   "udp://@239.255.12.4:1234")
-app.addStaion("NOVA",   "udp://@239.255.12.1:1234")
-app.addStaion("FANDA",  "udp://@239.255.14.4:1234")
-app.addStaion("CINEMA", "udp://@239.255.12.2:1234")
+app.addStation("ČT1 HD",            "udp://@239.255.15.1:1234")
+app.addStation("ČT2 HD",            "udp://@239.255.15.3:1234")
+app.addStation("ČT24",              "udp://@239.255.11.3:1234")
+app.addStation("ČT SPORT HD",       "udp://@239.255.15.2:1234")
+app.addStation("NOVA",              "udp://@239.255.12.1:1234")
+app.addStation("NOVA CINEMA",       "udp://@239.255.12.2:1234")
+app.addStation("PRIMA",             "udp://@239.255.12.3:1234")
+app.addStation("PRIMA COOL",        "udp://@239.255.12.4:1234")
+app.addStation("PRIMA ZOOM",        "udp://@239.255.13.3:1234")
+app.addStation("ČT1",               "udp://@239.255.11.1:1234")
+app.addStation("ČT2",               "udp://@239.255.11.2:1234")
+app.addStation("FANDA",             "udp://@239.255.14.4:1234")
+app.addStation("ČT Sport",          "udp://@239.255.11.4:1234")
+app.addStation("TV BARRANDOV",      "udp://@239.255.12.5:1234")
+app.addStation("PRIMA LOVE",        "udp://@239.255.13.1:1234")
+app.addStation("ÓČKO",              "udp://@239.255.13.2:1234")
+app.addStation("ÓČKO GOLD",         "udp://@239.255.13.6:1234")
+app.addStation("ACTIVE",            "udp://@239.255.13.7:1234")
+app.addStation("ČT D / ČT ART",     "udp://@239.255.13.8:1234")
+app.addStation("KINO BARRANDOV",    "udp://@239.255.13.9:1234")
+app.addStation("RELAX POHODA",      "udp://@239.255.14.3:1234")
+app.addStation("SMÍCHOV",           "udp://@239.255.14.5:1234")
+app.addStation("TELKA",             "udp://@239.255.14.6:1234")
+app.addStation("REBEL",             "udp://@239.255.14.7:1234")
+app.addStation("BTV",               "udp://@239.255.14.9:1234")
+app.addStation("RETRO MUSIC",       "udp://@239.255.15.5:1234")
+app.addStation("KINOSVET",          "udp://@239.255.15.6:1234")
+app.addStation("JOJ",               "udp://@239.255.16.1:1234")
+app.addStation("JOJ PLUS",          "udp://@239.255.16.2:1234")
+app.addStation("MARKÍZA",           "udp://@239.255.16.3:1234")
+app.addStation("DOMA",              "udp://@239.255.16.4:1234")
+app.addStation("TA3",               "udp://@239.255.16.5:1234")
+app.addStation("JOJ WAU",           "udp://@239.255.17.1:1234")
+app.addStation("8SEM",              "udp://@239.255.17.3:1234")
+app.addStation("JOJ SENZI",         "udp://@239.255.17.4:1234")
+app.addStation("STV: 1",            "udp://@239.255.20.1:1234")
+app.addStation("STV: 2",            "udp://@239.255.20.2:1234")
+app.addStation("STV: 1 HD",         "udp://@239.255.20.3:1234")
 app.runGUI()
-
